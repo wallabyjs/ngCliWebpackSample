9e12a44feeda744581804b55b86013b2df33ae46
Artem Govorov <artem.govorov@gmail.com>
2017-02-12 09:49:14 +1000

    Updated to angular-cli to beta 31

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { NgCliWebpackSample30Page } from './app.po';
+import { NgCliWebpackSample31Page } from './app.po';
 
-describe('ng-cli-webpack-sample30 App', function() {
-  let page: NgCliWebpackSample30Page;
+describe('ng-cli-webpack-sample31 App', function() {
+  let page: NgCliWebpackSample31Page;
 
   beforeEach(() => {
-    page = new NgCliWebpackSample30Page();
+    page = new NgCliWebpackSample31Page();
   });
 
   it('should display message saying app works', () => {
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class NgCliWebpackSample30Page {
+export class NgCliWebpackSample31Page {
   navigateTo() {
     return browser.get('/');
   }

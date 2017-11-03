3c53224f1adbecab23e03aacc3ab2e2676165f1d
Artem Govorov <artem.govorov@gmail.com>
2017-11-03 11:12:52 +1000

    Update wallabyTest.ts

--- src/wallabyTest.ts
+++ src/wallabyTest.ts
@@ -7,8 +7,6 @@ import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 
-[1].forEach(() => 1 === 1)
-
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,

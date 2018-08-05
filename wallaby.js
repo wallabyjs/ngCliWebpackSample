c051350a1dba8ec6269504307c97d81d0dad741e
Artem Govorov <artem.govorov@gmail.com>
2018-08-05 10:49:11 +1000

    Initial commit

--- wallaby.js
+++ wallaby.js
@@ -1,83 +1,29 @@
-var wallabyWebpack = require('wallaby-webpack');
-var path = require('path');
+module.exports = function () {
 
-var compilerOptions = Object.assign(
-  require('./tsconfig.json').compilerOptions,
-  require('./src/tsconfig.spec.json').compilerOptions);
-
-compilerOptions.module = 'CommonJs';
-
-module.exports = function (wallaby) {
-
-  var webpackPostprocessor = wallabyWebpack({
-    entryPatterns: [
-      'src/wallabyTest.js',
-      'src/**/*spec.js'
-    ],
-
-    module: {
-      rules: [
-        {test: /\.css$/, loader: ['raw-loader']},
-        {test: /\.html$/, loader: 'raw-loader'},
-        {test: /\.ts$/, loader: '@ngtools/webpack', include: /node_modules/, query: {tsConfigPath: 'tsconfig.json'}},
-        {test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/},
-        {test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader']},
-        {test: /\.less$/, loaders: ['raw-loader', {loader: 'less-loader', options: {paths: [__dirname]}}]},
-        {test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader']},
-        {test: /\.(jpg|png|svg)$/, loader: 'url-loader?limit=128000'}
-      ]
-    },
-
-    resolve: {
-      extensions: ['.js', '.ts'],
-      modules: [
-        path.join(wallaby.projectCacheDir, 'src/app'),
-        path.join(wallaby.projectCacheDir, 'src'),
-        'node_modules'
-      ]
-    },
-    node: {
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      dns: 'empty'
-    }
-  });
+  const jestTransform = file => require('jest-preset-angular/preprocessor').process(file.content, file.path, {globals: {__TRANSFORM_HTML__: true}, rootDir: __dirname});
 
   return {
     files: [
-      {pattern: 'src/**/*.+(ts|css|less|scss|sass|styl|html|json|svg)', load: false},
-      {pattern: 'src/**/*.d.ts', ignore: true},
-      {pattern: 'src/**/*spec.ts', ignore: true}
+      'src/**/*.+(ts|html|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
+      'jest.setup.ts',
+      '!src/**/*.spec.ts',
     ],
 
-    tests: [
-      {pattern: 'src/**/*spec.ts', load: false},
-      {pattern: 'src/**/*e2e-spec.ts', ignore: true}
-    ],
-
-    testFramework: 'jasmine',
-
-    compilers: {
-      '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
-    },
-
-    middleware: function (app, express) {
-      var path = require('path');
-      app.use('/favicon.ico', express.static(path.join(__dirname, 'src/favicon.ico')));
-      app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
-    },
+    tests: ['src/**/*.spec.ts'],
 
     env: {
-      kind: 'electron'
+      type: 'node',
+      runner: 'node'
     },
 
-    postprocessor: webpackPostprocessor,
-
-    setup: function () {
-      window.__moduleBundler.loadTests();
+    compilers: {
+      '**/*.html': file => ({code: jestTransform(file), map: {version: 3, sources: [], names: [], mappings: []}, ranges: []})
     },
 
-    debug: true
+    preprocessors: {
+      'src/**/*.js': jestTransform,
+    },
+
+    testFramework: 'jest'
   };
 };

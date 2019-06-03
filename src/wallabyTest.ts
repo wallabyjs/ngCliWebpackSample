f73281d8e0db5f1fa8ca94f19aa696b6bb4730e9
Artem Govorov <artem.govorov@gmail.com>
2019-06-03 15:42:06 +1000

    Updated wallaby test bootstrap to import correct core-js version

--- src/wallabyTest.ts
+++ src/wallabyTest.ts
@@ -1,6 +1,9 @@
 /* tslint:disable:ordered-imports file-name-casing */
 import './polyfills';
-import 'core-js/es7/reflect';
+
+// if you are using Angular < 8, then use `import 'core-js/es7/reflect'` instead;
+import '@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills';
+
 import 'zone.js/dist/zone-testing';
 
 import { getTestBed } from '@angular/core/testing';
